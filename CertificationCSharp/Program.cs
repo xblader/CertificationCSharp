10e79622040ca98fbf2b44cf5cecd736028d0875
Victor Hugo <xblader@gmail.com>
2015-09-01 00:04:40 -0300

    first example

--- CertificationCSharp/Program.cs
+++ CertificationCSharp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CertificationCSharp
@@ -10,7 +11,26 @@ namespace CertificationCSharp
     {
         static void Main(string[] args)
         {
-            //opa com oe que é?
+            //thread 2.0
+
+            Thread thread = new Thread(new ThreadStart(Run));
+            thread.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+            thread.Join();
+            Console.ReadKey();
+        }
+
+        public static void Run()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
         }
     }
 }
