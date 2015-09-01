d1f100e57af7194652888bade01c9101c881287d
Victor Hugo <xblader@gmail.com>
2015-08-31 22:04:39 -0300

    primeiro commit

---
+++ CertificationCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CertificationCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //opa com oe que é?
+        }
+    }
+}
